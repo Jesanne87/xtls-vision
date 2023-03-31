20e2a710dfb3a8de17fc433d91cca5a802eda5b2
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-31 17:57:06 +0800

    Update xtls-vision.sh

--- xtls-vision.sh
+++ xtls-vision.sh
@@ -273,103 +273,41 @@ success "Successfully installed Xray"
 	cat > $xray_conf << EOF
 {
   "log": {
-    "access": "/var/log/xray/access.log",
-    "error": "/var/log/xray/error.log",
+    "access": "$xray_access_log",
+    "error": "$xray_error_log",
     "loglevel": "warning"
   },
   "routing": {
+    "domainStrategy": "AsIs",
     "rules": [
       {
         "type": "field",
         "ip": [
-          "0.0.0.0/8",
-          "10.0.0.0/8",
-          "100.64.0.0/10",
-          "169.254.0.0/16",
-          "172.16.0.0/12",
-          "192.0.0.0/24",
-          "192.0.2.0/24",
-          "192.168.0.0/16",
-          "198.18.0.0/15",
-          "198.51.100.0/24",
-          "203.0.113.0/24",
-          "::1/128",
-          "fc00::/7",
-          "fe80::/10"
+          "geoip:private"
         ],
         "outboundTag": "block"
       },
       {
-        "inboundTag": [
-          "api"
-        ],
-        "outboundTag": "api",
-        "type": "field"
-      },
-      {
         "type": "field",
-        "outboundTag": "blocked",
-        "protocol": [
-          "bittorrent"
-        ]
+        "domain": [
+          "geosite:category-ads-all"
+        ],
+        "outboundTag": "block"
       }
     ]
   },
-  "stats": {},
-  "api": {
-    "services": [
-      "StatsService"
-    ],
-    "tag": "api"
-  },
-  "policy": {
-    "levels": {
-      "0": {
-        "statsUserDownlink": true,
-        "statsUserUplink": true
-      }
-    },
-    "system": {
-      "statsInboundUplink": true,
-      "statsInboundDownlink": true
-    }
-  }
-}  
   "inbounds": [
-        {
-      "listen": "127.0.0.1",
-      "port": 10085, # CEK USER QUOTA
-      "protocol": "dokodemo-door",
-      "settings": {
-        "address": "127.0.0.1"
-      },
-      "tag": "api"
-            },
-            {
+    {
       "port": $port,
       "protocol": "vless",
       "settings": {
         "clients": [
           {
             "id": "${passwd:-$uuid}",
-            "flow": "$xtls_flow",
-                        "level": 0
-#xray-vless-xtls
-#vxtls            
+            "flow": "$xtls_flow"
           }
         ],
-        "decryption": "none",
-                "fallbacks": [
-                    {
-                        "dest": "8001",
-                        "xver": 1
-                    },
-                    {
-                        "alpn": "h2",
-                        "dest": "8002",
-                        "xver": 1
-                    }
-                ]        
+        "decryption": "none"
       },
       "streamSettings": {
         "network": "tcp",
@@ -392,13 +330,12 @@ success "Successfully installed Xray"
   ],
   "outbounds": [
     {
-      "protocol": "freedom",
-      "settings": {}
+      "tag": "direct",
+      "protocol": "freedom"
     },
     {
-      "protocol": "blackhole",
-      "settings": {},
-      "tag": "blocked"
+      "tag": "block",
+      "protocol": "blackhole"
     }
   ]
 }
